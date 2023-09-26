93fdc7b1e52b2e840e94210442df5006b4721a09
Gaurav Pandey <gaurav.2022@tecorb.co>
2023-09-26 19:25:32 +0530

    landing page & tailwind properties

---
+++ src/component/fontandsizing/page.tsx
@@ -0,0 +1,55 @@
+export default function FontAndSize() {
+    return (
+
+        // font size and weight
+
+        // <div className="text-black">
+        //     <h1 className="text-2xl">Text h1</h1>
+        //     <h2 className="text-2xl font-mono tracking-wider">Text h2</h2>
+        //     <h6 className="text-2xl font-black tracking-widest">Text h6</h6>
+        // </div>
+
+
+        // space between
+
+        // <div className="text-black ">
+        //     <ul className="flex space-x-10">
+        //         <li>
+        //             Home
+        //         </li>
+        //         <li>
+        //             About
+        //         </li>
+        //         <li>
+        //             Contact Us
+        //         </li>
+        //     </ul>
+        // </div>
+
+
+        //Devide width
+
+        // <div className="text-black divide-x-8">
+        //     <div>
+                
+        //     </div>
+        //     <div>
+        //         01
+        //     </div>
+        //     <div>
+        //         02
+        //     </div>
+        //     <div>
+        //         03
+        //     </div>
+        // </div>
+
+
+
+        // square Width
+
+        <div className="mx-4 bg-red-800 my-[3rem] px-[10px]">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, ipsa aliquam asperiores aliquid ipsam ex commodi! Earum quae, temporibus deleniti qui consequuntur dignissimos?
+        </div>
+    )
+}
--- tailwind-css/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import FontAndSize from "@/component/fontandsizing/page";
-import LandingPage from "@/component/landingPage/page";
-
-export default function Home() {
-  return (
-    // <LandingPage />
-    <FontAndSize />
-  )
-}
