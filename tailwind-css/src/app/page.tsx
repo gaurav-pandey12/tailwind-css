83d45f4f59bd65fe05b42193145956eeda1a20e1
Gaurav Pandey <gaurav.2022@tecorb.co>
2023-09-26 19:19:36 +0530

    landing page & tailwind properties

---
+++ tailwind-css/src/app/page.tsx
@@ -0,0 +1,9 @@
+import FontAndSize from "@/component/fontandsizing/page";
+import LandingPage from "@/component/landingPage/page";
+
+export default function Home() {
+  return (
+    // <LandingPage />
+    <FontAndSize />
+  )
+}
